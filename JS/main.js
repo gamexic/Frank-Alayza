767f3f3ce653aab45557151ef0d7fcb41232b8e6
Frank Alayza <stephanoherrera4444@gmail.com>
2024-01-18 18:55:12 -0500

    Hacemos la seccion de portfolio y tambien ya responsive

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,20 @@ function toggleNavbar() {
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("active");
 }
+
+
+// TODO Main
+
+// * Portfolio
+
+function filterProjects(category) {
+    var projects = document.querySelectorAll('.project');
+
+    projects.forEach(project => {
+        if (category === 'all' || project.dataset.category === category) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+    });
+}
