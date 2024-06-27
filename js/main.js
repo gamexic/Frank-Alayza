ea4b8faeeb4bcf9f7365a3be441b6926378733a1
Frank <stephanoherrera4444@gmail.com>
2024-06-26 21:31:30 -0500

    Add: cv section y su archivo principal

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+// Función para que incluya el contenido de un archivo HTML en otro
+function includeHTML() {
+    var elements = document.getElementsByTagName('*');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var file = element.getAttribute('include-html');
+        if (file) {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        element.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        element.innerHTML = "Page not found.";
+                    }
+                    element.removeAttribute('include-html');
+                    includeHTML();
+                }
+            };
+            xhr.open('GET', file, true);
+            xhr.send();
+            return;
+        }
+    }
+}
+
+// Incluye el contenido de un archivo HTML en otro
+document.addEventListener('DOMContentLoaded', function () {
+    includeHTML();
+});
